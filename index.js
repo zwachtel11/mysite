3b1d2796d3da9db0edf8277d0fad4c16cafdacc6
zwachtel11 <zwachtel11@gmail.com>
2016-07-25 21:42:44 -0400

    testing1234

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ console.log("Express server listening on port "+ config.port);
 app.get('/',index);
 
 function index(req,res){
-   res.send('Hellooooo0123 ' + config.say);
+   res.send('4dbz in a cadilac: Coming Soon...');
  }
