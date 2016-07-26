175ba9dc28ad171ce6dbf2642ed296ef1413c330
zwachtel11 <zwachtel11@gmail.com>
2016-07-26 14:28:26 -0400

    test12345

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 'use strict'
 
 const express = require('express')
+const stylus = require('stylus')
+const nib = require('nib')
 const config = require('./configuration')
 
 const app = express()
 
+const compile = function(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib())
+}
+
 app.set('views', './views')
 app.set('view engine', 'pug')
+app.use(express.logger('dev'))
+app.use(stylus.middleware({
+  src: __dirname + '/public'
+  , compile: compile
+}))
+
+app.use(express.static(__dirname + '/public'))
 
 const index = function(req, res) {
   res.render('home', {
-    title: 'Welcome'
+    title: 'Coming Soon'
   })
 }
 
