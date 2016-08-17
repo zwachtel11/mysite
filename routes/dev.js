23defae231a354f823aab2423ae94a8a251f8d23
zwachtel11 <zwachtel11@gmail.com>
2016-08-17 15:34:38 -0400

    yo

--- routes/dev.js
+++ routes/dev.js
@@ -4,7 +4,7 @@ const router = express.Router()
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.render('index', {title: 'Dev'})
+  res.render('dev', {title: 'Dev'})
 })
 
 module.exports = router
