90333881079c91dd1f0274fd2070da4363a8c2a4
zwachtel11 <zwachtel11@gmail.com>
2016-09-04 11:26:54 -0500

    update

--- routes/contact.js
+++ routes/contact.js
@@ -4,7 +4,7 @@ const router = express.Router()
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.render('contact', {title: 'contact'})
+  res.render('contact', {title: 'contact', header1: 'Contact Me'})
 })
 
 module.exports = router
