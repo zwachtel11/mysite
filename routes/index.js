b886cb4d2cb63dd95f095bd13f23fb2561840140
zwachtel11 <zwachtel11@gmail.com>
2016-09-12 15:35:22 -0500

    update

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router()
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', {title: 'Welcome', header1: 'Welcome To Wachs Tech Blog'})
+  res.render('index', {title: 'Welcome', header2: 'Welcome To Wachs Tech Blog'})
 })
 
 module.exports = router
